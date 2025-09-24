d2a72a25ded80a8b05777638bd8440744f5e197e
João Horta Alves <joao.horta.alves@gmail.com>
2025-09-24 11:13:13 +0000

    Add sub querying to document retrieval

--- src/gradio_ui.py
+++ src/gradio_ui.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 from textwrap import dedent
 
 import gradio as gr
@@ -14,17 +15,24 @@ try:
     llm_mini_json = llm_mini.with_structured_output(method="json_mode")
     system_prompt = dedent("""\
     Instructions:
-    Answer the following user query based on the provided context.
-    If the answer is not in the context, check if it is in the Chat History, and answer based on that.
+    Answer the following User Query based on the provided Context.
+    If the answer is not in the Context, check if it is in the Chat History, and answer based on that.
     If you still can't find an answer, say "That is out of my scope".
-    Use the provided metadata to give sources of the context you used on your answer, if a source is available.""").strip()
+    Use the provided Metadata to give sources of the Context used on the answer, if a source is available.""").strip()
 
 
     def inference_function(user_query, history):
-        sub_query_prompt = dedent("""\
-        
-        """).strip()
-        relevant_documents = vector_store.similarity_search_with_score(user_query, k=2)
+        sub_query_prompt = dedent(f"""\
+        Given the following User Query, check if there are multiple questions being asked.
+        If there are return each question separately in a JSON array.
+        Otherwise return the JSON array with a single element which is the User Query verbatim.
+        The JSON object should have a key named "questions" and a value which is a JSON array of questions.
+        User Query:
+        {user_query}""").strip()
+        sub_queries = llm_mini_json.invoke(sub_query_prompt)["questions"]
+        print(f"sub_queries: {sub_queries}")
+        relevant_documents = [doc for sub_query in sub_queries for doc in
+                              vector_store.similarity_search_with_score(sub_query, k=2)]
         print(f"relevant_documents score: {[score for _, score in relevant_documents]}")
         relevant_documents = [document for document, score in relevant_documents if score < 0.9]
         print(f"relevant_documents after score filtering: {relevant_documents}")
